d7efceaf5bfa46b2b73cbb9cbafbf2de0c403f33
Abdullahi Mohamud <38002443+Abdullahi254@users.noreply.github.com>
2024-01-03 22:47:18 +0300

    resolving and rejecting promises

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default const getResponseFromAPI = (success)=> {
+  return new Promise((res, rej) => {
+    if (success){
+      res({
+        status: 200,
+        body: 'Success'
+      })
+    }
+    else {
+      rej("The fake API is not working currently");
+    }
+  });
+}
