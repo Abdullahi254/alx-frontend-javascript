bf79bc2d80ea87705489833b35b7f293c32cc8e3
Abdullahi Mohamud <38002443+Abdullahi254@users.noreply.github.com>
2024-01-03 17:52:50 +0300

    Update 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let [index, value] of array.entries()) {
-    array[index] = appendString + value;
+  // eslint-disable-next-line
+  for (const value of array) {
+    // eslint-disable-next-line
+    array[array.indexOf(value)] = appendString + value;
   }
 
   return array;
