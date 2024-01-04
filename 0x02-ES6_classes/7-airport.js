e281ad6851ed3134898f5ab4392422140ef4cf67
Abdullahi Mohamud <38002443+Abdullahi254@users.noreply.github.com>
2024-01-04 15:04:29 +0300

    Create 7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,16 @@
+class Airport {
+  constructor(name, code) {
+    if(typeof(name) !=="string"){
+      throw TypeError("name must be a string!");
+    }
+    if(typeof(code) !=="string"){
+      throw TypeError("code must be a string!");
+    }
+    this._name = name;
+    this._code = code;
+  }
+  toString() {
+    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+  }
+  
+}
