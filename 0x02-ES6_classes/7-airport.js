39a8aa3e857685e9dfbcd583599fd0662394e6e6
Abdullahi Mohamud <38002443+Abdullahi254@users.noreply.github.com>
2024-01-04 15:16:26 +0300

    Update 7-airport.js

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,18 +1,16 @@
-class Airport {
+export default class Airport {
   constructor(name, code) {
-    if(typeof(name) !=="string"){
-      throw TypeError("name must be a string!");
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
     }
-    if(typeof(code) !=="string"){
-      throw TypeError("code must be a string!");
-    }
+
     this._name = name;
     this._code = code;
   }
-  toString() {
-    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
-  }
-  
-}
 
-export default Airport;
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+}
