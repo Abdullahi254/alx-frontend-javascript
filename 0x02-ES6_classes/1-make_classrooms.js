29373ccfe52a7f1fcb4a55a89680335ee136157b
Abdullahi Mohamud <38002443+Abdullahi254@users.noreply.github.com>
2024-01-04 13:17:48 +0300

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,3 +1,5 @@
 import ClassRoom from './0-classroom.js';
 
-const initializeRooms = ()=> [19, 20, 34].map(val=> new ClassRoom(val));
+const initializeRooms = ()=>{
+  return [19, 20, 34].map(val=> new ClassRoom(val));
+} 
