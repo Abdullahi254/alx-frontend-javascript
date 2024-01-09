6971da813d13a8aec0d139829a85bb46c87d46bc
Abdullahi Mohamud <38002443+Abdullahi254@users.noreply.github.com>
2024-01-09 21:48:04 +0300

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,3 @@
-export default const getStudentsByLocation = (list, city)=> {
-  if (list.length > 0) {
-    return list.filter(obj=>list.location === city);
-  }
-  return [];
+export default function getStudentsByLocation(lst, city) {
+  return lst.filter((obj) => obj.location === city);
 }
